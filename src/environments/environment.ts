c289fb6f63cf7d868c48d6ba9b1b9d1bd4876439
josedavila2745 <josedavila2745@gmail.com>
2020-12-22 01:44:16 -0500

    listo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBZiHtDvVLo6_eYOceHo575CYUupa8-wVo",
+    authDomain: "youplaza-186ab.firebaseapp.com",
+    projectId: "youplaza-186ab",
+    storageBucket: "youplaza-186ab.appspot.com",
+    messagingSenderId: "698803182971",
+    appId: "1:698803182971:web:2294d7a7559b45ea0a71de",
+    measurementId: "G-E6SFGCLG3F"
+  }
 };
 
 /*
