49093ed2dd65ec85d30d15c0e80ebc90309a70e0
josedavila2745 <josedavila2745@gmail.com>
2020-12-22 01:11:49 -0500

    ya

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {AngularFireModule} from '@angular/fire';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
